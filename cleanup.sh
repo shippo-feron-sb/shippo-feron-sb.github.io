20c61dc1546a03bd01daf4938abaf7c83ad6b5a8
Sudipta Sen <feron-storybook@shippo.com>
2024-05-13 16:51:45 -0500

    added workflow

--- cleanup.sh
+++ cleanup.sh
@@ -3,6 +3,7 @@ for dir in ./*/
 do
     DATEOFLAST=`git log -n 1 --pretty=format:%ct ${dir}`
     FIVE_DAYS_AGO=$(date +%s -d '5 days ago')
+    echo "Date of last commit: ${DATEOFLAST}"
     if [[ $DATEOFLAST < $FIVE_DAYS_AGO && $dir != "./master/" ]]
     then
         HAS_CLEANUP=true
